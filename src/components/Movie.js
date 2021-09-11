619cf632357fd3847fdd582f647f1ba4f648aa94
samahhamed <85107833+samahhamed227@users.noreply.github.com>
2021-09-11 23:42:32 +0300

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,103 +1,48 @@
-"usee strict";
-import "./App.css";
-import axios from "axios";
 import React from "react";
-import Weather from "./components/weather";
-import Movie from "./components/Movie";
-import "./App.css";
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchQuery: "",
-      locData: "",
-      displayed: false,
-      errormessege: false,
-      weatherData: [],
-      locationMovie: [],
-    };
-  }
-
-  getlocation = async (e) => {
-    e.preventDefault();
-
-    try {
-      let locurl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.searchQuery}&format=json`;
-
-      let locationres = await axios.get(locurl);
-
-      console.log(locationres.data[0]);
-      this.setState({
-        locData: locationres.data[0],
-        displayed: true,
-      });
-      let axiosLocalApi = `${process.env.REACT_APP_SERVER_URL}/weather?city=${this.state.searchQuery}`;
-
-      let axiosresult = await axios.get(axiosLocalApi);
-      this.setState({
-        weatherData: axiosresult.data,
-      });
-      const moviesaxios = await axios.get(
-        `${process.env.REACT_APP_SERVER_URL}/movies?query=${this.state.searchQuery}`
-      );
-
-      this.setState({
-        locationMovie: moviesaxios.data,
-      });
-    } catch {
-      this.setState({
-        displayed: false,
-        errormessege: true,
-      });
-    }
-  };
-
-  updates = (event) => {
-    this.setState({
-      searchQuery: event.target.value,
-    });
-    console.log(this.state.searchQuery);
-  };
-
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+class Movie extends React.Component {
   render() {
+    console.log(this.props);
     return (
-      <div style={{ backgroundColor: "beige" }}>
-        <h1 style={{ textAlign: "center" }}>City Explorer</h1>
+      <>
+         <Row className="justify-content-md-center" id='moviesRow'>
 
-        <form onSubmit={this.getlocation} style={{ marginLeft: "550px" }}>
-          <input type="text" placeholder="Add a city" onChange={this.updates} />
-          <input type="submit" value="Get Location" />
-        </form>
-        <p style={{ textAlign: "center" }}>{this.state.locData.display_name}</p>
-        <p style={{ textAlign: "center" }}>{this.state.locData.lat}</p>
-        <p style={{ textAlign: "center" }}>{this.state.locData.lon}</p>
-
-        {this.state.displayed && (
-          <img
-            src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.state.locData.lat},${this.state.locData.lon}`}
-            style={{ margin: "150px", width: "1000px" }}
-            alt="map"
-          />
-        )}
-        <div>
-          <Weather
-            weatherData={this.state.weatherData}
-            searchQuery={this.state.searchQuery}
-          />
-        </div>
-        <div>
-          <Movie locationMovie={this.state.locationMovie} />
-        </div>
-
-        {this.state.errormessege && (
-          <p style={{ textAlign: "center", color: "red" }}>
-            erroing in the data
-          </p>
-        )}
-      </div>
+        {this.props.locationMovie.map((element) => {
+          return (
+            <Col>
+            <Card
+              style={{
+                width: "16rem",
+                border: "solid",
+            
+                margin: "5px",
+              }}
+            >
+              <Card.Body
+                style={{
+                  color: "black",
+                }}
+              >
+                <Card.Title>Movie</Card.Title>
+                <Card.Text>{element.title}</Card.Text>
+                <Card.Text>{element.overview}</Card.Text>
+                <Card.Text>{element.vote}</Card.Text>
+                <Card.Text>{element.count}</Card.Text>
+                <Card.Text>{element.release_date}</Card.Text>
+                <Card.Text>{element.popularity}</Card.Text>
+                <Card.Img variant="top" src={element.img_url} alt={element.title}  />
+              </Card.Body>
+            </Card>
+            </Col>
+          );
+        })}
+        </Row>
+      </>
     );
   }
 }
 
-
-export default App;
+export default Movie;
